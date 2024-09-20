7953248bb4eac20d5512f98b48964a14024ac3da
deepak gupta <79707576+speedytech121@users.noreply.github.com>
2024-09-20 03:24:50 +0000

    update search insert pos code

--- binary-search/search_insert_pos.py
+++ binary-search/search_insert_pos.py
@@ -1,18 +1,16 @@
 def search_insert_pos(arr,target):
     low=0
     high=len(arr)-1
-    position=0
+    position=len(arr)
 
     while (low<=high):
         mid = (low+high)//2
-        if arr[mid]<target:
-            position=mid+1
-            low=mid+1
-        elif arr[mid]==target:
-            return mid
-        else:
+        if arr[mid]>=target:
+            position=mid
             high=mid-1
-    
+        else:
+            low=mid+1
+        
     return position
 
 print(search_insert_pos([1,2,4,5,6],3))
