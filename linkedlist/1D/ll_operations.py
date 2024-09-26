798bf44c194047654f9e95c638004e688a0a5e14
deepak gupta <79707576+speedytech121@users.noreply.github.com>
2024-09-26 03:49:30 +0000

    updated ll methods with few new functions

--- linkedlist/1D/ll_operations.py
+++ linkedlist/1D/ll_operations.py
@@ -3,57 +3,122 @@ class Node:
         self.data=data
         self.next=None
 
-class LinkedList:
+# insert at start, end, search, print, delete, update, insert at index
+class Linkedlist:
     def __init__(self):
         self.head=None
     
-    def add_start(self,data):
-        new_node=Node(data)
-        new_node.next=self.head
-        self.head=new_node
+    def insertatstart(self, data):
+        newnode=Node(data)
+        newnode.next=self.head
+        self.head=newnode
 
-    def add_end(self,data):
-        new_node=Node(data)
-        if not self.head:
-            self.head=new_node
-            return 
-        last=self.head
-        while last.next:
-            last=last.next
-        last.next=new_node
-
-    def findlength(self):
-        count=0
+    def insertatend(self,data):
+        newnode=Node(data)
         current=self.head
-        while current:
+        while current.next!=None:
+            current=current.next
+        current.next=newnode
+
+    def insertatindex(self,data, index):
+        newnode=Node(data)
+        if self.head is None:
+            print("list is empty")
+            return
+        if index==0:
+            newnode.next=self.head
+            self.head=newnode
+            return
+        
+        current=self.head
+        previous=None
+        count=0
+        while count<index:
+            previous=current
             current=current.next
             count+=1
-        print(f'length of ll is: {count}')
+        
+        previous.next=newnode
+        newnode.next=current
 
-    def searchelement(self,target):
+
+        
+    def deleteatindex(self, index):
+        if self.head is None:
+            print("list is empty")
+            return
+        
+        if index==0:
+            self.head=self.head.next
+            return
+        
+        current=self.head
+        previous=None
+        count=0
+
+        while current and count<index:
+            previous = current
+            current = current.next
+            count+=1
+
+        if current is None:
+            print("index out of range")
+            return
+        previous.next=current.next
+
+
+    def updateatindex(self,data, index):
+        self.deleteatindex(index)
+        self.insertatindex(data,index)
+        
+    
+    def searchelement(self,data):
+        current=self.head
+        index=0
+        while current:
+            if current.data==data:
+                return index
+            current=current.next
+            index+=1 
+
+    def printll(self):
         current=self.head
         while current:
-            if current.data==target:
-                print(target)
-                return
+            print(current.data)
             current=current.next
 
-    def print_ll(self):
-        current=self.head
-        while current:
-            print(current.data, end='->')
-            current=current.next
-        print("None")
+obj=Linkedlist()
+obj.insertatstart(4)
+obj.insertatstart(3)
+obj.insertatstart(2)
+obj.insertatstart(1)
 
-ll=LinkedList()
-ll.add_start(3)
-ll.add_start(2)
-ll.add_start(1)
+print("LL after inserting at start-")
+obj.printll()
 
-ll.add_end(4)
-ll.add_end(5)
-ll.add_end(6)
+obj.insertatend(5)
+obj.insertatend(6)
 
-ll.print_ll()
-ll.findlength()
-ll.searchelement(5)
+print("LL after inserting at end-")
+obj.printll()
+
+obj.deleteatindex(2)
+print("ll after deletion at index")
+obj.printll()
+
+
+obj.insertatindex(3,2)
+obj.insertatindex(31,3)
+obj.insertatindex(32,4)
+print("insert at index")
+obj.printll()
+
+
+obj.updateatindex(30,2)
+print("update at index")
+obj.printll()
+
+
+print("searching an element")
+print(obj.searchelement(30))
+print(obj.searchelement(32))
